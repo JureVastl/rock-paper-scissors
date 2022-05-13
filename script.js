986c4420fec3a4cd8f9c19887aa8e63aaaf82b1b
Jure Vastl <vastl.jure@gmail.com>
2022-05-13 18:14:29 +0200

    add round and game functions
    - Added the playRound Function
    -Added game function that payls the game for 5 rounds

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
 //global variables
 const choices = ["rock", "paper", "scissors"];
-const playerSelection = "rock";
-const computerSelection = computerPlay();
 
 //get computer input-choice
 function computerPlay() {
   return choices[Math.floor(Math.random() * choices.length)];
+}
+
+//Play round function
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection = "rock";
+  computerSelection = computerPlay();
+
+  if (playerSelection === computerSelection) {
+    console.log("It is a Tie!");
+  } else if (
+    (playerSelection === "rock" && computerSelection == "scissors") ||
+    (playerSelection === "paper" && computerSelection == "rock") ||
+    (playerSelection === "scissors" && computerSelection == "paper")
+  ) {
+    console.log("You Win");
+  } else {
+    console.log("Computer won!");
+  }
+}
+
+//Play 5 rounds of the game
+function game() {
+  for (let i = 0; i < 5; i++) {
+    playRound();
+  }
+}
