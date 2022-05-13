d33c6acf323f61ea70627b1bcd204970a4ae49b1
Jure Vastl <vastl.jure@gmail.com>
2022-05-13 19:56:10 +0200

    add player input and validation
    - added playerPlay function wich prompts the player for the choices. 
    -Added validation function for player choices
    - Cleaned up console log

--- script.js
+++ script.js
@@ -6,22 +6,59 @@ function computerPlay() {
   return choices[Math.floor(Math.random() * choices.length)];
 }
 
-//Play round function
+//get player input-choice
+function playerPlay() {
+  let input = prompt("Type in Rock, Paper or Scissors");
+  while (input == null) {
+    input = prompt("Type in Rock, Paper or Scissors");
+  }
+  input = input.toLowerCase();
+  let check = validateInput(input);
+  while (check == false) {
+    input = prompt(
+      "Error! Please type only one of the following: Rock, Paper or Scissors"
+    );
+    while (input == null) {
+      input = prompt("Type in Rock, Paper or Scissors");
+    }
+    input = input.toLowerCase();
+    check = validateInput(input);
+  }
+  return input;
+}
 
+//validate the input of the player
+function validateInput(choice) {
+  return choices.includes(choice);
+}
+
+//Play round function
 function playRound(playerSelection, computerSelection) {
-  playerSelection = "rock";
+  playerSelection = playerPlay();
   computerSelection = computerPlay();
 
   if (playerSelection === computerSelection) {
-    console.log("It is a Tie!");
+    console.log("Both chose " + playerSelection + "\nIt is a Tie!");
   } else if (
     (playerSelection === "rock" && computerSelection == "scissors") ||
     (playerSelection === "paper" && computerSelection == "rock") ||
     (playerSelection === "scissors" && computerSelection == "paper")
   ) {
-    console.log("You Win");
+    console.log(
+      "Player chose " +
+        playerSelection +
+        "\nComputer chose " +
+        computerSelection +
+        "\nYou Win!"
+    );
   } else {
-    console.log("Computer won!");
+    console.log(
+      "Player chose " +
+        playerSelection +
+        "\nComputer chose " +
+        computerSelection +
+        "\nComputer Wins!"
+    );
   }
 }
 
